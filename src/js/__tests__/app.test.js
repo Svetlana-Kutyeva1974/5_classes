ef9136dd68d0d47af897c659df381e7c8e3057ee
Svetlana Kutyova <kutyovas@yandex.ru>
2021-10-28 18:40:43 +0300

    1

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,27 @@
+import * as heroes from '../app.js';
+
+test.each([
+  ['hero1', 'Bowman', new heroes.Character('hero1', 'Bowman')],
+  ['hero2', 'Bowman', new heroes.Bowman('hero2', 'Bowman')],
+  ['hero3', 'Magician', new heroes.Magician('hero3', 'Magician')],
+  ['hero4', 'Magician', new heroes.Magician('hero4', 'Magician')],
+  ['hero5', 'Magician', new heroes.Magician('hero5', 'Magician')],
+  ['hero6', 'Magician', new heroes.Magician('hero6', 'Magician')],
+  ['hero7', 'Magician', new heroes.Magician('hero7', 'Magician')],
+])(
+  'should return a string corresponding to the health level',
+  (name, type, createNewObject) => {
+    for (const key of Object.values(heroes)) {
+      const K = key;
+      const result = new K(name, type);
+      expect(result).toEqual(createNewObject);
+    }
+  },
+);
+
+/* test('person return value live level :wounded', () => {
+  expect(person({ name: 'Маг', health: 45 })).toBe('wounded');
+});
+test('person return value live level : critical', () => {
+  expect(person({ name: 'Лучник', health: 10 })).toBe('critical');
+}); */
---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+﻿export class Character {
+static TYPES = {
+    'Bowman': [ 25,  25 ],
+    'Swordsman': [40,  10 ],
+    'Magician': [ 10,  40 ], 
+    'Daemon': [10,  40 ], 
+    'Undead': [ 25,  25 ], 
+    'Zombie': [ 40, 10 ]
+  }
+
+  constructor(name, type) { //
+    if (name.length > 10 || name.length < 2) throw new Error("Ошибка, имя должно содержать не менее 2 и не более 10 символов");
+    if (!Object.keys(Character.TYPES).includes(type)) throw new Error("Ошибка, недопустимый тип элемента");
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = Character.TYPES[`${type}`][0];
+    this.defence = Character.TYPES[`${type}`][1];
+  }
+}
+
+export class Bowman extends Character {
+}
+export class Magician extends Character {
+}
+export class Swordsman extends Character {
+}
+export class Daemon extends Character {
+}
+export class Zombie extends Character {
+}
+export class Undead extends Character {
+}
